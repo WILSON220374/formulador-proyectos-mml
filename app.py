f2372c0cd9c0791440e345a7dfb1ffa341599450
WILSON220374 <wesanchezh@gmail.com>
2026-02-17 20:22:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -142,10 +142,6 @@ pg = st.navigation({
     "Fase III: Análisis del problema": [
         st.Page("views/9_descripcion_zona.py", title="9. Descripción de la Zona", icon="🗺️"),
         st.Page("views/10_descripcion_problema.py", title="10. Descripción del Problema", icon="📝"), # <--- NUEVA PÁGINA
-    ],
-  "Fase IV: Análisis de Objetivos": [
-        st.Page("views/11_indicadores.py", title="11. Indicadores", icon="📈"),
     ]
 })
-
 pg.run()
